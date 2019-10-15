9c6a086424914e93494a66024f5871e17678286d
ColdFerrin <ferrinkatz@ferrinkatz.com>
2019-10-15 12:39:47 -0700

    switch from powerlevel9k to powerlevel10k

--- .zshrc
+++ .zshrc
@@ -8,7 +8,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -74,13 +74,6 @@ source $ZSH/oh-my-zsh.sh
 source ~/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source_z_temp
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs aws)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
-
-#Shorten directory shown
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-POWERLEVEL9K_SHORTEN_STRATEGY=”truncate_folders”
-
 #Set default user to avoid showing 'user' on every line
 DEFAULT_USER="user_temp"
 
@@ -118,3 +111,5 @@ alias rf="rm -rf"
 
 prompt_context () { }
 
+# to customize prompt, run `p10k configure` or edit ~/.p10k.zsh
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
--- build.sh
+++ build.sh
@@ -128,8 +128,8 @@ installStuff () {
     printf "\033[1;32m-- install zsh syntax highlighting\033[0m\n"
     cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
-    printf "\033[1;32m-- install powerlevel9k oh my zsh theme\033[0m\n"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    printf "\033[1;32m-- install powerlevel10k oh my zsh theme\033[0m\n"
+    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
     printf "\033[1;32m-- install powerline fonts\033[0m\n"
     cd
@@ -177,8 +177,8 @@ installStuff () {
     sudo apt install -y openjdk-8-jdk
     sudo apt install -y openjdk-11-jdk
 
-    printf "\033[1;32m-- install powerlevel9k oh my zsh theme\033[0m\n"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    printf "\033[1;32m-- install powerlevel10k oh my zsh theme\033[0m\n"
+    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
     chmod 755 oh-my-zsh.sh
 
     printf "\033[1;32m-- install powerline fonts\033[0m\n"
@@ -226,8 +226,8 @@ installStuff () {
     sudo yum install -y java-1.8.0-openjdk-devel
     sudo yum install -y java-11-openjdk-devel
 
-    printf "\033[1;32m-- install powerlevel9k oh my zsh theme\033[0m\n"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    printf "\033[1;32m-- install powerlevel10k oh my zsh theme\033[0m\n"
+    git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
     chmod 755 oh-my-zsh.sh
 
     printf "\033[1;32m-- install powerline fonts\033[0m\n"
@@ -364,7 +364,7 @@ fi
 cd "$parent_path"
 if [[ ${doBuild} -eq 1 ]]; then
   buildZshrc ${platform} ${bashName}
-  yes | sudo cp -f .zshrc .gitignore .vimrc ~/.
+  yes | sudo cp -f .zshrc .gitignore .vimrc .p10k.zsh ~/.
 fi
 
 if [[ ${doSSH} -eq 1 ]]; then
